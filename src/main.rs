4e00b58451a2f9c09564a117a96d92534a476603
Michiel Borkent <michielborkent@gmail.com>
2020-03-13 11:00:47 +0100

    glob wip

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use tree_sitter::{Parser, Language, Node};
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
+use glob::glob;
+use std::fs::metadata;
 
 extern "C" { fn tree_sitter_clojure() -> Language; }
 
@@ -13,7 +15,7 @@ fn print_node(node: Node, bytes: &[u8]) {
     println!("{}",node_text(node, bytes));
 }
 
-fn print_reify(node: Node, bytes: &[u8]) {
+fn print_reify_usage(node: Node, bytes: &[u8]) {
     let text = node_text(node, &bytes);
     if text == "reify" {
         let prev_sibling = node.prev_sibling().unwrap();
@@ -28,9 +30,23 @@ fn print_reify(node: Node, bytes: &[u8]) {
         //println!("{:?}", node.kind());
         for child_num in 0..child_count {
             let child = node.child(child_num).unwrap();
-            print_reify(child, &bytes);
+            print_reify_usage(child, &bytes);
         }
     }
+}
+
+fn paths_from_arg(arg: &String) -> glob::Paths {
+    println!("{}", arg);
+    let md = metadata(arg).unwrap();
+    let is_dir = md.is_dir();
+    println!("{}", is_dir);
+    let pat = if is_dir {
+        String::from(arg) + "/**"
+    } else {
+        String::from(arg)
+    };
+    println!("{}", pat);
+    glob(&pat).unwrap()
 }
 
 fn main() {
@@ -40,13 +56,21 @@ fn main() {
 
     //let args: Vec<String> = vec![String::from("core.clj")];
     let args: Vec<String> = env::args().collect();
-    for filename in args.iter().skip(1) {
-        let mut f = File::open(filename).unwrap();
-        let mut source_code = String::new();
-        f.read_to_string(&mut source_code).unwrap();
-        let bytes = source_code.as_bytes();
-        let tree = parser.parse(&source_code, None).unwrap();
-        let root_node = tree.root_node();
-        print_reify(root_node, &bytes);
+    let path = &args[1];
+
+    for entry in paths_from_arg(path) {
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
     }
+    // for filename in args.iter().skip(1) {
+    //     let mut f = File::open(filename).unwrap();
+    //     let mut source_code = String::new();
+    //     f.read_to_string(&mut source_code).unwrap();
+    //     let bytes = source_code.as_bytes();
+    //     let tree = parser.parse(&source_code, None).unwrap();
+    //     let root_node = tree.root_node();
+    //     print_reify_usage(root_node, &bytes);
+    // }
 }
