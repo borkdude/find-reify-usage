e2e947610d9d21d03819fe5d29e08f4bd23a5c31
Michiel Borkent <michielborkent@gmail.com>
2020-03-18 20:55:42 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use tree_sitter::{Parser, Language, Node};
-use std::path::{Path, PathBuf};
-use std::env;
 use glob::glob;
-use std::fs::metadata;
 use rayon::prelude::*;
+use std::env;
+use std::path::{Path, PathBuf};
+use std::sync::Mutex;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::{SystemTime};
+use tempdir::TempDir;
+use tree_sitter::{Parser, Language, Node};
 use zip::read::{ZipFile};
-use std::sync::Mutex;
 
 extern "C" { fn tree_sitter_clojure() -> Language; }
+
+struct AppCfg<'a> {
+    atomic_counter: &'a AtomicUsize,
+    tmp_dir: &'a Path,
+}
 
 fn node_text<'a>(node: Node<'a>, bytes: &'a [u8]) -> &'a str {
     node.utf8_text(bytes).unwrap()
@@ -48,7 +53,7 @@ fn print_reify_usage_from_bytes(bytes: &[u8]) {
     print_reify_usage_from_node(root_node, &bytes);
 }
 
-fn print_reify_usage_from_file_path(path: &Path) {
+fn print_reify_usage_from_file_path(path: &Path, app_cfg: &AppCfg) {
     //println!("-- print_reify_usage_from_file -- Processing path: {:?}", path);
     let language: Language = unsafe { tree_sitter_clojure() };
     let mut parser = Parser::new();
@@ -58,6 +63,7 @@ fn print_reify_usage_from_file_path(path: &Path) {
     let bytes = contents.as_bytes();
     let tree = parser.parse(&bytes, None).unwrap();
     let root_node = tree.root_node();
+    app_cfg.atomic_counter.fetch_add(1, Ordering::Relaxed);
     print_reify_usage_from_node(root_node, &bytes);
 }
 
@@ -65,27 +71,18 @@ fn print_reify_usage_from_file_path(path: &Path) {
 // https://github.com/mvdnes/zip-rs/blob/master/examples/extract.rs
 // http://siciarz.net/24-days-rust-zip-and-lzma-compression/
 
-trait ReadToString {
-    fn read_to_string(&mut self, s: &mut String);
+fn read_to_string(tmp_dir: &Path, f: &mut ZipFile, s: &mut String) {
+    let outpath = f.sanitized_name();
+    let outpath = tmp_dir.join(outpath);
+    let parent = outpath.parent().unwrap();
+    std::fs::create_dir_all(&parent).unwrap();
+    let mut outfile = std::fs::File::create(&outpath).unwrap();
+    std::io::copy(f, &mut outfile).unwrap();
+    let contents = std::fs::read_to_string(outpath.as_path()).unwrap();
+    s.push_str(&contents);
 }
 
-impl ReadToString for ZipFile<'_> {
-    fn read_to_string(&mut self, s: &mut String) {
-        let outpath = self.sanitized_name();
-        let outpath = std::path::PathBuf::from("/tmp/").join(outpath);
-        println!("OUTPATH: {:?}", outpath);
-        let parent = outpath.parent().unwrap();
-        std::fs::create_dir_all(&parent).unwrap(); // ah, this is a directory, now I get it!
-        let mut outfile = std::fs::File::create(&outpath).unwrap(); // Err: is a directory
-        //println!("OUTFILE: {:?}", outfile);
-        std::io::copy(self, &mut outfile).unwrap();
-        let contents = std::fs::read_to_string(outpath.as_path()).unwrap();
-        s.push_str(&contents);
-        //std::fs::remove_dir_all(&parent).unwrap();
-    }
-}
-
-fn print_reify_usage_from_zipfile_path(path: &Path, atomic_counter: &AtomicUsize) {
+fn print_reify_usage_from_zipfile_path(path: &Path, app_cfg: &AppCfg) {
     //println!("Processing zip: {:?}", path);
     let file = std::fs::File::open(&path).unwrap();
     let archive = zip::ZipArchive::new(file).unwrap();
@@ -94,59 +91,72 @@ fn print_reify_usage_from_zipfile_path(path: &Path, atomic_counter: &AtomicUsize
     range.into_par_iter().for_each(|i| {
         let mut archive = mutex.lock().unwrap();
         let mut file = archive.by_index(i).unwrap();
-        if (file.name()).ends_with(".clj") {
-            let outpath = file.sanitized_name();
-            if !(file.is_dir()) {
-                let mut contents = String::new();
-                file.read_to_string(&mut contents);
-                atomic_counter.fetch_add(1, Ordering::Relaxed);
-                println!("Analyzing:      {:?}", outpath);
-                let bytes = &contents.as_bytes();
-                print_reify_usage_from_bytes(&bytes);
-                println!("Done analyzing: {:?}", outpath);
-            }
+        if file.is_file() && (file.name()).ends_with(".clj") {
+            //let outpath = file.sanitized_name();
+            let mut contents = String::new();
+            read_to_string(app_cfg.tmp_dir, &mut file, &mut contents);
+            app_cfg.atomic_counter.fetch_add(1, Ordering::Relaxed);
+            //println!("Analyzing:      {:?}", outpath);
+            let bytes = &contents.as_bytes();
+            print_reify_usage_from_bytes(&bytes);
+            //println!("Done analyzing: {:?}", outpath);
         }
     });
 }
 
-fn paths_from_arg(arg: &String) -> glob::Paths {
-    let md = metadata(arg).unwrap();
-    let is_dir = md.is_dir();
-    let pat = if is_dir {
-        String::from(arg) + "/**"
-    } else {
-        String::from(arg)
-    };
-    glob(&pat).unwrap()
+fn print_reify_usage_from_dir(path: &Path, app_cfg: &AppCfg) {
+    let mut str = path.to_str().unwrap().to_owned();
+    str.push_str("/**/*.*");
+    //println!("dir: {:?}", str);
+    let paths: Vec<Result<PathBuf,_>> = glob(&str).unwrap().collect();
+    //println!("{:?}", paths);
+    paths.into_par_iter().for_each(|path| {
+        print_reify_dispatch(path.unwrap().as_path(), app_cfg);
+    });
+}
+
+
+fn print_reify_dispatch(path: &Path, app_cfg: &AppCfg) {
+    //println!("dispatch: {:?}", path);
+    if path.is_dir() {
+        print_reify_usage_from_dir(path, app_cfg);
+        return;
+    }
+    if path.is_file() {
+        match path.extension() {
+            None => return,
+            Some(ext) => {
+                if ext == "jar" {
+                    print_reify_usage_from_zipfile_path(path, app_cfg);
+                    return;
+                }
+                if ext == "clj" {
+                    print_reify_usage_from_file_path(path, app_cfg);
+                }
+            }
+        }
+    }
+
 }
 
 fn main() {
     let start = SystemTime::now();
     let args: Vec<String> = env::args().skip(1).collect();
     let atomic_counter = AtomicUsize::new(0);
-    args.into_par_iter().for_each(|arg| {
-        let paths: Vec<Result<PathBuf,_>> = paths_from_arg(&arg).collect();
-        paths.into_par_iter().for_each(|entry| {
-            match entry {
-                Ok(path) => {
-                    let path = path.as_path();
-                    //println!("Processing path: {:?}, {:?}, {:?}", path, path.is_file(), path.extension().unwrap());
-                    if path.is_file() {
-                        if path.extension().unwrap() == "clj" {
-                            atomic_counter.fetch_add(1, Ordering::Relaxed);
-                            print_reify_usage_from_file_path(path);
-                        } else if path.extension().unwrap() == "jar" {
-                            print_reify_usage_from_zipfile_path(path, &atomic_counter);
-                        }
-                    }
-                },
-                Err(e) => panic!(format!("Unexpected error while analyzing {}", e))
-            }
-        });
+    let tmp_dir = TempDir::new("analyze-reify").unwrap();
+    let tmp_dir = tmp_dir.path();
+    let app_cfg = AppCfg { atomic_counter: &atomic_counter, tmp_dir: &tmp_dir };
+    let paths: Vec<&Path> = args.iter().map(|arg| {
+        Path::new(arg)
+    }).collect();
+    paths.into_par_iter().for_each(|path| {
+        print_reify_dispatch(&path, &app_cfg);
     });
     let since_start = SystemTime::now().duration_since(start)
         .expect("Time went backwards");
     eprintln!("Processed {} files in {}ms. 😎"
               , atomic_counter.load(Ordering::SeqCst)
               , since_start.as_millis());
+    // eprintln!("Cleaning up {:?}", &tmp_dir);
+    std::fs::remove_dir_all(&tmp_dir).unwrap();
 }
