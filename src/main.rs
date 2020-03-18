46c114d89efcc4534e3f8ad7c22a733295f5885b
Michiel Borkent <michielborkent@gmail.com>
2020-03-18 14:39:04 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn print_reify_usage_from_node(node: Node, bytes: &[u8]) {
 }
 
 fn print_reify_usage_from_file_path(path: &Path) {
-    println!("-- print_reify_usage_from_file -- Processing path: {:?}", path);
+    //println!("-- print_reify_usage_from_file -- Processing path: {:?}", path);
     let language: Language = unsafe { tree_sitter_clojure() };
     let mut parser = Parser::new();
     parser.set_language(language).unwrap();
@@ -62,17 +62,19 @@ trait ReadToString {
 impl ReadToString for ZipFile<'_> {
     fn read_to_string(&mut self, s: &mut String) {
         let outpath = self.sanitized_name();
-        println!("Outpath: {:?}", outpath);
-        std::fs::create_dir_all(&outpath).unwrap();
+        //println!("OUTPATH: {:?}", outpath);
+        let parent = outpath.parent().unwrap();
+        std::fs::create_dir_all(&parent).unwrap(); // ah, this is a directory, now I get it!
         let mut outfile = std::fs::File::create(&outpath).unwrap(); // Err: is a directory
+        //println!("OUTFILE: {:?}", outfile);
         std::io::copy(self, &mut outfile).unwrap();
         let contents = std::fs::read_to_string(outpath.as_path()).unwrap();
         s.push_str(&contents);
     }
 }
 
-fn print_reify_usage_from_zipfile_path(path: &Path) {
-    println!("Processing zip: {:?}", path);
+fn print_reify_usage_from_zipfile_path(path: &Path, atomic_counter: &AtomicUsize) {
+    //println!("Processing zip: {:?}", path);
     let file = std::fs::File::open(&path).unwrap();
     let mut archive = zip::ZipArchive::new(file).unwrap();
     for i in 0..archive.len() {
@@ -80,12 +82,12 @@ fn print_reify_usage_from_zipfile_path(path: &Path) {
         if (&*file.name()).ends_with(".clj") {
             let outpath = file.sanitized_name();
             if !(file.is_dir()) {
-                println!("Processing zip clj! {:?}", file.name());
-                println!("Data start:{:?}", file .data_start());
+                //println!("Processing zip clj! {:?}", file.name());
+                //println!("Data start:{:?}", file .data_start());
                 let mut contents = String::new();
                 file.read_to_string(&mut contents);
+                atomic_counter.fetch_add(1, Ordering::Relaxed);
                 print_reify_usage_from_file_path(&outpath);
-                std::fs::create_dir_all(&outpath).unwrap();
             }
         }
     }
@@ -112,13 +114,13 @@ fn main() {
             match entry {
                 Ok(path) => {
                     let path = path.as_path();
-                    println!("Processing path: {:?}, {:?}, {:?}", path, path.is_file(), path.extension().unwrap());
+                    //println!("Processing path: {:?}, {:?}, {:?}", path, path.is_file(), path.extension().unwrap());
                     if path.is_file() {
                         if path.extension().unwrap() == "clj" {
                             atomic_counter.fetch_add(1, Ordering::Relaxed);
                             print_reify_usage_from_file_path(path);
                         } else if path.extension().unwrap() == "jar" {
-                            print_reify_usage_from_zipfile_path(path);
+                            print_reify_usage_from_zipfile_path(path, &atomic_counter);
                         }
                     }
                 },
